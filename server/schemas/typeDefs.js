3482b972ebfdadb779eda008006b4ec7f322d261
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-02-07 18:57:31 +0800

    add typdef schemas and first query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,41 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    input BookInput {
+        authors: String
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    typre Auth {
+        token: ID!
+        user: User
+    }
+
+    typer Query {
+        user(username: String!): User
+    }
+
 
 
 `;
